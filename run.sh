d89a47451971e095b61131d52bf57b1696ee4887
WDPS course <wdps1811@fs0.cm.cluster>
2018-11-26 23:53:57 +0100

    [Modify] run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
-#./run.sh WARC-TREC-ID hdfs:///user/bbkruit/sample.warc.gz
+#./run.sh WARC-TREC-ID hdfs:///user/wdps1811/sample.warc.gz
 
-if [ $# -ne 2 ]; then
-	echo $0: Usage: \<Warc-key\> \<Input-File-Path\> 
-	exit 1
-fi
+#if [ $# -ne 2 ]; then
+#	echo $0: Usage: \<Warc-key\> \<Input-File-Path\> 
+#	exit 1
+#fi
 
 if [ "$SPARK_HOME" = "" ]; then
 	echo SPARK_HOME not set. Please create the environment variable SPARK_HOME pointing to your Spark home directory.
@@ -12,3 +12,5 @@ if [ "$SPARK_HOME" = "" ]; then
 fi
 
 PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit --master local[*] a1.py $1 $2
+PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit --master local[*] a1.py  WARC-TREC-ID hdfs:///user/wdps1811/sample.warc.gz
+
