8ae186e3ff35f6450961df39774cf5b37c17c654
WDPS course <wdps1811@fs0.cm.cluster>
2018-11-27 19:48:28 +0100

    [Modify] get the print result from RDD

--- a1.py
+++ a1.py
@@ -27,11 +27,12 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 sc = SparkContext.getOrCreate()
 
 # Check input parameters
-if len(sys.argv) < 3 or len(sys.argv) > 3:
-    print('Usage - <Warc_key> <Input_file>')
+if len(sys.argv) < 4 or len(sys.argv) > 4:
+    print('Usage - <Warc_key> <Input_file> <output_file>')
 else:
     record_attribute = sys.argv[1]
     in_file = sys.argv[2]
+    out_file = sys.argv[3]
 
 
 # Read warc file and split in WARC/1.0
@@ -101,215 +102,218 @@ def processWarcfile(record):
 rdd_pairs = rdd.flatMap(processWarcfile)  # RDD with tuples (key, text)
 # Save plain text before transform it for disambiguation
 rdd_disambiguation = rdd_pairs
+print(rdd_pairs.collect())
+rdd_print = rdd_pairs.saveAsTextFile(out_file)
 
 #NLP - NER
 # 1. Tokenization
 # 2. NER - StanfordNER
 
 
-def NLP_NER(record):
-    #sent_text = nltk.sent_tokenize(record)
-    tokenized_text = nltk.word_tokenize(record)
-    #FIXME TypeError: a bytes-like object is required, not 'str'
-    #tokenized_text = [x.encode('utf-8') for x in tokenized_text]
-    tag_text = nltk.pos_tag(tokenized_text)
+# def NLP_NER(record):
+#     #sent_text = nltk.sent_tokenize(record)
+#     tokenized_text = nltk.word_tokenize(record)
+#     #FIXME TypeError: a bytes-like object is required, not 'str'
+#     #tokenized_text = [x.encode('utf-8') for x in tokenized_text]
+#     tag_text = nltk.pos_tag(tokenized_text)
 
-    # StanfordNER
-    ner_text_NER = nlp.tag(tokenized_text)  # Option 1 - Word tokenization
-    # ner_text = [nlp.tag(s.split()) for s in sent_text] #Option 2 - Sentece tokenization
+#     # StanfordNER
+#     ner_text_NER = nlp.tag(tokenized_text)  # Option 1 - Word tokenization
+#     # ner_text = [nlp.tag(s.split()) for s in sent_text] #Option 2 - Sentece tokenization
 
-    yield ner_text_NER
-
-
-# StanfordNERTagger - Files needed
-# Path may change
-classifier = '/home/wdps1811/scratch/lib/stanford-ner-2018-10-16/classifiers/english.all.3class.distsim.crf.ser.gz'
-# Path may change
-jar = '/home/wdps1811/scratch/lib/stanford-ner-2018-10-16/stanford-ner.jar'
-nlp = StanfordNERTagger(classifier, jar)
-
-# RDD tuples (key, tuple(word, label))
-rdd_ner = rdd_pairs.flatMapValues(NLP_NER)
-
-# Entity extraction
-# Extract Name Entities from result - Function to get recognized entities from Stanford NER
-def get_entities_StanfordNER(record):
-    entities = []
-    for i in record:
-        if (i[1] != 'O' and i[0] not in entities):
-            entities.append(i[0])
-
-    yield entities
-
-# Extract Muliterm Name Entities from result - Function to get recognized entities from Stanford NER
+#     yield ner_text_NER
 
 
-def get_entities_StanfordNER_multiterm(record):
-    entities = []
-    last_tag = None
-    for i in record:
-        if (i[1] != 'O' and i[0] not in entities):
-            if i[1] == last_tag:
-                if i[0] not in entities[len(entities)-1]:
-                    entities[len(entities) -
-                             1] = entities[len(entities)-1]+' '+i[0]
-            else:
-                entities.append(i[0])
-        last_tag = i[1]
+# # StanfordNERTagger - Files needed
+# # Path may change
+# classifier = '/home/wdps1811/scratch/lib/stanford-ner-2018-10-16/classifiers/english.all.3class.distsim.crf.ser.gz'
+# # Path may change
+# jar = '/home/wdps1811/scratch/lib/stanford-ner-2018-10-16/stanford-ner.jar'
+# nlp = StanfordNERTagger(classifier, jar)
 
-    yield entities
+# # RDD tuples (key, tuple(word, label))
+# rdd_ner = rdd_pairs.flatMapValues(NLP_NER)
 
+# # Entity extraction
+# # Extract Name Entities from result - Function to get recognized entities from Stanford NER
+# def get_entities_StanfordNER(record):
+#     entities = []
+#     for i in record:
+#         if (i[1] != 'O' and i[0] not in entities):
+#             entities.append(i[0])
 
-rdd_ner_entities = rdd_ner.flatMapValues(
-    get_entities_StanfordNER)  # RDD tuples (key, entities)
+#     yield entities
 
-
-# Link entities to KB
-ELASTICSEARCH_URL = 'http://10.149.0.127:9200/freebase/label/_search'
-
-# Get IDs, label and score from ELASTICSEARCH for each entity
+# # Extract Muliterm Name Entities from result - Function to get recognized entities from Stanford NER
 
 
-def get_elasticsearch(record):
-    tuples = []
-    for i in record:
-        query = i
-        response = requests.get(ELASTICSEARCH_URL, params={
-                                'q': query, 'size': 100})  # Query all the entities
-        result = {}
-        if response:
-            response = response.json()
-            for hit in response.get('hits', {}).get('hits', []):
-                freebase_id = hit.get('_source', {}).get('resource')
-                label = hit.get('_source', {}).get('label')
-                score = hit.get('_score', 0)
+# def get_entities_StanfordNER_multiterm(record):
+#     entities = []
+#     last_tag = None
+#     for i in record:
+#         if (i[1] != 'O' and i[0] not in entities):
+#             if i[1] == last_tag:
+#                 if i[0] not in entities[len(entities)-1]:
+#                     entities[len(entities) -
+#                              1] = entities[len(entities)-1]+' '+i[0]
+#             else:
+#                 entities.append(i[0])
+#         last_tag = i[1]
 
-                if result.get(freebase_id) == None:  # Check duplicate id
-                    # If freebase_id is not in the dict, add all the extract info from JSON
-                    result[freebase_id] = (
-                        {'label': label, 'score': score, 'facts': 0, 'match': 0, 'text': '', 'similarity': 0})
-                else:
-                    score_1 = max(result[freebase_id]['score'], score)
-                    # If freebase_id is in the dict, update the score but not create a new entry
-                    result[freebase_id]['score'] = score_1
-        # Return entity with its associated dictionary with the info from elastic search query
-        tuples.append([i, result])
-    yield tuples
+#     yield entities
 
 
-# RDD (key, [entity, dict{freebase_id: {score,  label}}])
-rdd_labels = rdd_ner_entities.flatMapValues(get_elasticsearch)
+# rdd_ner_entities = rdd_ner.flatMapValues(
+#     get_entities_StanfordNER)  # RDD tuples (key, entities)
 
 
-# Link IDs to motherKB
-TRIDENT_URL = 'http://10.141.0.125:9001/sparql'  # May change
-prefixes = """
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX fbase: <http://rdf.freebase.com/ns/>
-"""
-same_as_template = prefixes + """
-SELECT DISTINCT ?same WHERE {
-    ?s owl:sameAs %s .
-    { ?s owl:sameAs ?same .} UNION { ?same owl:sameAs ?s .}
-}
-"""
-po_template = prefixes + """
-SELECT DISTINCT * WHERE {
-    %s ?p ?o.
-}
-"""
-# Get number of facts from KB for each entity. Calculate the 'match' from score and facts as stated in https://github.com/bennokr/wdps2017/query.py
+# # Link entities to KB
+# ELASTICSEARCH_URL = 'http://10.149.0.127:9200/freebase/label/_search'
+
+# # Get IDs, label and score from ELASTICSEARCH for each entity
 
 
-def get_motherKB(record):
-    tuples = []
-    for i in record:
-        entity = i[0]
-        for key in i[1]:
-            response = requests.post(
-                TRIDENT_URL, data={'print': False, 'query': po_template % key})
-            if response:
-                response = response.json()
-                n = int(response.get('stats', {}).get('nresults', 0))
-                i[1][key]['facts'] = n
-                if n != 0:
-                    i[1][key]['match'] = math.log(n) * i[1][key]['score']
-                else:
-                    i[1][key]['match'] = 0
-        tuples.append([entity, i[1]])
-    yield tuples
+# def get_elasticsearch(record):
+#     tuples = []
+#     for i in record:
+#         query = i
+#         response = requests.get(ELASTICSEARCH_URL, params={
+#                                 'q': query, 'size': 100})  # Query all the entities
+#         result = {}
+#         if response:
+#             response = response.json()
+#             for hit in response.get('hits', {}).get('hits', []):
+#                 freebase_id = hit.get('_source', {}).get('resource')
+#                 label = hit.get('_source', {}).get('label')
+#                 score = hit.get('_score', 0)
+
+#                 if result.get(freebase_id) == None:  # Check duplicate id
+#                     # If freebase_id is not in the dict, add all the extract info from JSON
+#                     result[freebase_id] = (
+#                         {'label': label, 'score': score, 'facts': 0, 'match': 0, 'text': '', 'similarity': 0})
+#                 else:
+#                     score_1 = max(result[freebase_id]['score'], score)
+#                     # If freebase_id is in the dict, update the score but not create a new entry
+#                     result[freebase_id]['score'] = score_1
+#         # Return entity with its associated dictionary with the info from elastic search query
+#         tuples.append([i, result])
+#     yield tuples
 
 
-rdd_ids = rdd_labels.flatMapValues(get_motherKB)
+# # RDD (key, [entity, dict{freebase_id: {score,  label}}])
+# rdd_labels = rdd_ner_entities.flatMapValues(get_elasticsearch)
 
 
-# Get 5 best matches from the complete set of results and perform entity disambiguation with the 5 best results
-def get_bestmatches(record):
-    best_matches = {}
-    tuples = []
-    links = []
-    for i in record:
-        entity = i[0]
-        best_matches = dict(
-            sorted(i[1].items(), key=lambda x: (x[1]['match']), reverse=True)[:5])
-        for key in best_matches:
-            response = requests.post(
-                TRIDENT_URL, data={'print': True, 'query': same_as_template % key})
-            if response:
-                response = response.json()
-                for binding in response.get('results', {}).get('bindings', []):
-                    link = binding.get('same', {}).get('value', None)
-                    if link.startswith('http://dbpedia.org'):
-                        html = urllib.urlopen(link)
-                        if html.getcode() == 200:
-                            html = html.read()
-                            link_text = get_text(html, 1)
-                            best_matches[key]['text'] = link_text
-        tuples.append([entity, best_matches])
-    yield tuples
+# # Link IDs to motherKB
+# TRIDENT_URL = 'http://10.141.0.125:9001/sparql'  # May change
+# prefixes = """
+# PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+# PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+# PREFIX owl: <http://www.w3.org/2002/07/owl#>
+# PREFIX fbase: <http://rdf.freebase.com/ns/>
+# """
+# same_as_template = prefixes + """
+# SELECT DISTINCT ?same WHERE {
+#     ?s owl:sameAs %s .
+#     { ?s owl:sameAs ?same .} UNION { ?same owl:sameAs ?s .}
+# }
+# """
+# po_template = prefixes + """
+# SELECT DISTINCT * WHERE {
+#     %s ?p ?o.
+# }
+# """
+# # Get number of facts from KB for each entity. Calculate the 'match' from score and facts as stated in https://github.com/bennokr/wdps2017/query.py
 
 
-rdd_ids_dis = rdd_ids.flatMapValues(get_bestmatches)
+# def get_motherKB(record):
+#     tuples = []
+#     for i in record:
+#         entity = i[0]
+#         for key in i[1]:
+#             response = requests.post(
+#                 TRIDENT_URL, data={'print': False, 'query': po_template % key})
+#             if response:
+#                 response = response.json()
+#                 n = int(response.get('stats', {}).get('nresults', 0))
+#                 i[1][key]['facts'] = n
+#                 if n != 0:
+#                     i[1][key]['match'] = math.log(n) * i[1][key]['score']
+#                 else:
+#                     i[1][key]['match'] = 0
+#         tuples.append([entity, i[1]])
+#     yield tuples
 
 
-# Choose the best result by calculating cosine similarity between the extracted text and the dbpedia text for the entity
-def cos_similarity(record):
-    tuples = []
-    vect = TfidfVectorizer(min_df=1)
-    for i in record[0]:
-        entity = i[0]
-        for key in i[1]:
-            if i[1][key]['text'] != '':
-                text = i[1][key]['text']
-                tfidf = vect.fit_transform([text, record[1]])
-                pairwise_similarity = tfidf * tfidf.T
-                i[1][key]['similarity'] = pairwise_similarity[0, 1]
-        result = dict(sorted(i[1].items(), key=lambda x: (
-            x[1]['similarity']), reverse=True)[:1])
-        tuples.append([entity, result])
-    yield tuples
+# rdd_ids = rdd_labels.flatMapValues(get_motherKB)
 
 
-rdd_result = rdd_ids_dis.join(rdd_disambiguation)
-rdd_result = rdd_result.flatMapValues(cos_similarity)
+# # Get 5 best matches from the complete set of results and perform entity disambiguation with the 5 best results
+# def get_bestmatches(record):
+#     best_matches = {}
+#     tuples = []
+#     links = []
+#     for i in record:
+#         entity = i[0]
+#         best_matches = dict(
+#             sorted(i[1].items(), key=lambda x: (x[1]['match']), reverse=True)[:5])
+#         for key in best_matches:
+#             response = requests.post(
+#                 TRIDENT_URL, data={'print': True, 'query': same_as_template % key})
+#             if response:
+#                 response = response.json()
+#                 for binding in response.get('results', {}).get('bindings', []):
+#                     link = binding.get('same', {}).get('value', None)
+#                     if link.startswith('http://dbpedia.org'):
+#                         html = urllib.urlopen(link)
+#                         if html.getcode() == 200:
+#                             html = html.read()
+#                             link_text = get_text(html, 1)
+#                             best_matches[key]['text'] = link_text
+#         tuples.append([entity, best_matches])
+#     yield tuples
 
-# Write the output to a file
+
+# rdd_ids_dis = rdd_ids.flatMapValues(get_bestmatches)
 
 
-def get_output(record):
-    s = ''
-    if record[1]:
-        for i in record[1]:
-            for key in i[1]:
-                key = key.split(':')[1]
-                key = key.replace(".", "/")
-                s += record[0]+"\t\t\t"+i[0]+"\t\t\t/"+key+"\n"
-    return s
+# # Choose the best result by calculating cosine similarity between the extracted text and the dbpedia text for the entity
+# def cos_similarity(record):
+#     tuples = []
+#     vect = TfidfVectorizer(min_df=1)
+#     for i in record[0]:
+#         entity = i[0]
+#         for key in i[1]:
+#             if i[1][key]['text'] != '':
+#                 text = i[1][key]['text']
+#                 tfidf = vect.fit_transform([text, record[1]])
+#                 pairwise_similarity = tfidf * tfidf.T
+#                 i[1][key]['similarity'] = pairwise_similarity[0, 1]
+#         result = dict(sorted(i[1].items(), key=lambda x: (
+#             x[1]['similarity']), reverse=True)[:1])
+#         tuples.append([entity, result])
+#     yield tuples
 
 
-result = rdd_result.coalesce(1).map(get_output)
-#FIXME the output.tsv is a folder??? should be a file
-result.saveAsTextFile('output_l.tsv')
-print('The output is the file part-00000 in the directory output.tsv')
+# rdd_result = rdd_ids_dis.join(rdd_disambiguation)
+# rdd_result = rdd_result.flatMapValues(cos_similarity)
+
+# # Write the output to a file
+
+
+# def get_output(record):
+#     s = ''
+#     if record[1]:
+#         for i in record[1]:
+#             for key in i[1]:
+#                 key = key.split(':')[1]
+#                 key = key.replace(".", "/")
+#                 s += record[0]+"\t\t\t"+i[0]+"\t\t\t/"+key+"\n"
+#     return s
+
+
+# result = rdd_result.coalesce(1).map(get_output)
+# #FIXME the output.tsv is a folder? yes, the ture output is inside the floder
+# result.saveAsTextFile('output_floder')
+# result.saveAsTextFile(out_file)
+# print('The output is the file part-00000 in the directory output.tsv')
--- run.sh
+++ run.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 #./run.sh WARC-TREC-ID hdfs:///user/wdps1811/sample.warc.gz
 
-#if [ $# -ne 2 ]; then
-#	echo $0: Usage: \<Warc-key\> \<Input-File-Path\> 
-#	exit 1
-#fi
+# if [ $# -ne 2 ]; then
+# 	echo $0: Usage: \<Warc-key\> \<Input-File-Path\> 
+# 	exit 1
+# fi
+
+SCRIPT=${1:-"a1.py"}
+WARCID=${2:-"WARC-TREC-ID"}
+INFILE=${3:-"hdfs:///user/wdps1811/sample.warc.gz"}
+OUTFILE=${4:-"templeResult"}
 
 if [ "$SPARK_HOME" = "" ]; then
 	echo SPARK_HOME not set. Please create the environment variable SPARK_HOME pointing to your Spark home directory.
-	exit 1
+	exit 1ls
+
 fi
 
-PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit --master local[*] a1.py $1 $2
-PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit --master local[*] a1.py  WARC-TREC-ID hdfs:///user/wdps1811/sample.warc.gz
+# PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit --master local[*] a1.py $1 $2
+# PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit --master local[*] a1.py "WARC-TREC-ID" "hdfs:///user/wdps1811/sample.warc.gz"
+PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit --master local[*] $SCRIPT $WARCID $INFILE $OUTFILE
 
+hdfs dfs -cat $OUTFILE"/*" > $OUTFILE
