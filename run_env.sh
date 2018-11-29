d1b0a8edf1d7fb0a66386c3564e905c3bebb0f29
WDPS course <wdps1811@fs0.cm.cluster>
2018-11-29 17:51:33 +0100

    [Modify] change the run_venv.sh

--- run_env.sh
+++ run_env.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 2 ]; then
-	echo $0: Usage: \<Warc-key\> \<Input-File-Path\> 
-	exit 1
-fi
+#if [ $# -ne 2 ]; then
+#	echo $0: Usage: \<Warc-key\> \<Input-File-Path\> 
+#	exit 1
+#fi
+
+SCRIPT=${1:-"a1.py"}
+WARCID=${2:-"WARC-TREC-ID"}
+INFILE=${3:-"hdfs:///user/wdps1811/sample.warc.gz"}
+OUTFILE=${4:-"templeResult"}
 
 if [ "$SPARK_HOME" = "" ]; then
 	echo SPARK_HOME not set. Please create the environment variable SPARK_HOME pointing to your Spark home directory.
@@ -20,4 +25,9 @@ PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit \
 --master yarn \
 --deploy-mode cluster \
 --archives venv.zip#VENV \
-A1_WDPS1708.py $1 $2
+$SCRIPT $WARCID $INFILE $OUTFILE
+
+hdfs dfs -cat $OUTFILE"/*" > $OUTFILE
+
+
+#A1_WDPS1708.py $1 $2
