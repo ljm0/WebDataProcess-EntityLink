9d4859624995ccef861a16a7d437d49fd751ae08
WDPS course <wdps1811@fs0.cm.cluster>
2018-12-07 20:36:50 +0100

    [Modify] new elsticsearch, right version

--- run.sh
+++ run.sh
@@ -7,52 +7,32 @@
 # fi
 
 ES_PORT=9200
-ES_BIN=/home/bbkruit/scratch/wdps/elasticsearch-2.4.1/bin/elasticsearch
+# ES_BIN=/home/bbkruit/scratch/wdps/elasticsearch-2.4.1/bin/elasticsearch
+ES_BIN=$(realpath ~/scratch/elasticsearch-2.4.1/bin/elasticsearch)
 
 prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
 echo "waiting 15 seconds for elasticsearch to set up..."
-sleep 15
-ES_NODE=$(cat .es_node | grep '^:' | grep -oP '(node...)')
+until [ -n "$ES_NODE" ]; do ES_NODE=$(cat .es_node | grep '^:' | grep -oP '(node...)'); done
 ES_PID=$!
+sleep 15
 echo "elasticsearch should be running now on node $ES_NODE:$ES_PORT (connected to process $ES_PID)"
-
-# sleep 30
-
-# prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
-# echo "waiting 15 seconds for elasticsearchBackup1 to set up..."
-# sleep 15
-# ES_NODE1=$(cat .es_node | grep '^:' | grep -oP '(node...)')
-# ES_PID1=$!
-# echo "elasticsearch_backup1 should be running now on node $ES_NODE1:$ES_PORT (connected to process $ES_PID1)"
-
-# sleep 60
-
-# prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
-# echo "waiting 15 seconds for elasticsearchBackup2 to set up..."
-# sleep 15
-# ES_NODE2=$(cat .es_node | grep '^:' | grep -oP '(node...)')
-# ES_PID2=$!
-# echo "elasticsearch_backup2 should be running now on node $ES_NODE2:$ES_PORT (connected to process $ES_PID2)"
 
 SCRIPT=${1:-"a1.py"}
 WARCID=${2:-"WARC-TREC-ID"}
 INFILE=${3:-"hdfs:///user/wdps1811/sample.warc.gz"}
-OUTFILE=${4:-"templeResultLIU"}
+OUTFILE=${4:-"templeResultLIU02"}
 ELASTICSEARCH=${5:-"$ES_NODE:$ES_PORT"}
-# ELASTICSEARCH1=${6:-"$ES_NODE:$ES_PORT"}
-# ELASTICSEARCH2=${7:-"$ES_NODE:$ES_PORT"}
 
-if [ "$SPARK_HOME" = "" ]; then
-	echo SPARK_HOME not set. Please create the environment variable SPARK_HOME pointing to your Spark home directory.
-	exit 1ls
+# if [ "$SPARK_HOME" = "" ]; then
+# 	echo SPARK_HOME not set. Please create the environment variable SPARK_HOME pointing to your Spark home directory.
+# 	exit 1ls
 
-fi
+# fi
 export PYTHONPATH=$PYTHONPATH:/home/wdps1811/scratch/lib
 # PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit --master local[*] a1.py $1 $2
 # PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit --master local[*] a1.py "WARC-TREC-ID" "hdfs:///user/wdps1811/sample.warc.gz"
 PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit --master local[*] $SCRIPT $WARCID $INFILE $OUTFILE $ELASTICSEARCH
-#$ELASTICSEARCH1 $ELASTICSEARCH2
 
 hdfs dfs -cat $OUTFILE"/*" > $OUTFILE
-# remove the result to project generating  same name file
+# remove the result to project generating same name file
 hdfs dfs -rm -r  hdfs:///user/wdps1811/$OUTFILE
--- run_env.sh
+++ run_env.sh
@@ -4,24 +4,28 @@
 #	echo $0: Usage: \<Warc-key\> \<Input-File-Path\> 
 #	exit 1
 #fi
-export PYTHONPATH=$PYTHONPATH:/home/wdps1811/scratch/lib
+ES_PORT=9200
+# ES_BIN=/home/bbkruit/scratch/wdps/elasticsearch-2.4.1/bin/elasticsearch
+ES_BIN=$(realpath ~/scratch/elasticsearch-2.4.1/bin/elasticsearch)
+
+prun -o .es_log -v -np 1 ESPORT=$ES_PORT $ES_BIN </dev/null 2> .es_node &
+echo "waiting 15 seconds for elasticsearch to set up..."
+until [ -n "$ES_NODE" ]; do ES_NODE=$(cat .es_node | grep '^:' | grep -oP '(node...)'); done
+ES_PID=$!
+sleep 15
+echo "elasticsearch should be running now on node $ES_NODE:$ES_PORT (connected to process $ES_PID)"
 
 SCRIPT=${1:-"a1.py"}
 WARCID=${2:-"WARC-TREC-ID"}
 INFILE=${3:-"hdfs:///user/wdps1811/sample.warc.gz"}
-OUTFILE=${4:-"templeResult"}
+OUTFILE=${4:-"templeResultLIU02"}
 ELASTICSEARCH=${5:-"$ES_NODE:$ES_PORT"}
-
-if [ "$SPARK_HOME" = "" ]; then
-	echo SPARK_HOME not set. Please create the environment variable SPARK_HOME pointing to your Spark home directory.
-	exit 1
-fi
 # Create virtual environment and install all necessary dependencies
 
 source venv/bin/activate
 virtualenv --relocatable venv
 zip -r venv.zip venv
-
+export PYTHONPATH=$PYTHONPATH:/home/wdps1811/scratch/lib
 PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit \
 --conf spark.yarn.appMasterEnv.PYSPARK_PYTHON=./VENV/venv/bin/python \
 --master yarn \
@@ -30,6 +34,5 @@ PYSPARK_PYTHON=$(readlink -f $(which python)) $SPARK_HOME/bin/spark-submit \
 $SCRIPT $WARCID $INFILE $OUTFILE $ELASTICSEARCH
 
 hdfs dfs -cat $OUTFILE"/*" > $OUTFILE
-
-
-#A1_WDPS1708.py $1 $2
+# remove the result to project generating same name file
+hdfs dfs -rm -r  hdfs:///user/wdps1811/$OUTFILE
